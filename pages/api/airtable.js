b7d6c37e84b61e1195cbd14248f50e6cf7950baf
nagasimasyou <nagashimasyou0919@gmail.com>
2025-07-26 23:35:53 +0900

    Add files via upload

---
+++ pages/api/airtable.js
@@ -0,0 +1,8 @@
+import Airtable from 'airtable'
+
+const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base('app7NTBQ6xAipe6gi')
+
+export default async function handler(req, res) {
+  const records = await base('Imported table').select({}).all()
+  res.status(200).json({ records })
+}
