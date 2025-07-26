bf9e6384a37c3ecb4f444e332035118d6db9332e
nagasimasyou <nagashimasyou0919@gmail.com>
2025-07-26 23:22:25 +0900

    Update airtable.js

--- pages/api/airtable.js
+++ pages/api/airtable.js
@@ -4,6 +4,6 @@ import Airtable from 'airtable'
 const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base('app7NTBQ6xAipe6gi')
 
 export default async function handler(req, res) {
-  const records = await base('seizika-karute').select({}).all()
+  const records = await base('Imported table').select({}).all()
   res.status(200).json({ records })
 }
